bbaa2c148a31c5dc313ed92ba3c85a7ce3e2f219
seo-hyeon kim <winterjoker341@gmail.com>
2024-06-23 15:21:18 +0900

    feat: Impl rank part

--- pages/rank_page.py
+++ pages/rank_page.py
@@ -1,10 +1,24 @@
+import sys
 import pygame
 from pygame.locals import *
+from element.rank.rank import Rank
+from element.base.text import Text
+from data.rank import rank_data
 
 def rank_page(screen, clock):
+    title = Text("Rank", (350, 100), 42, (0, 0, 255))
+
+    rank_list = []
+    for i, score in enumerate(rank_data):
+        rank_list.append(Rank(i+1, score, [350, i*120+250], [500, 80], (0, 255, 0)))
+
     running = True
     while running:
-        screen.fill((0, 255, 0))
+        screen.fill((255, 255, 255))
+
+        title.draw(screen)
+        for rank in rank_list:
+            rank.draw(screen)
 
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
